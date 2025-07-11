7531cbffd6a2c91cf7f7631c8cd5e80919fc90f8
songhantran <hantransong@gmail.com>
2025-07-11 23:42:41 +0700

    Add files via upload

--- TruyXuatNguonGoc.sol
+++ TruyXuatNguonGoc.sol
@@ -4,6 +4,17 @@ pragma solidity ^0.8.0;
 contract TruyXuatNguonGoc {
     address public owner;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier chiChuSoHuu() {
+        require(msg.sender == owner, "Chi chu so huu moi co quyen");
+        _;
+    }
+
+    // ----------- PHẦN 1: DỮ LIỆU SẢN PHẨM -----------
+
     struct SanPham {
         string tenSanPham;
         string noiSanXuat;
@@ -21,15 +32,6 @@ contract TruyXuatNguonGoc {
         string ipfsHash
     );
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    modifier chiChuSoHuu() {
-        require(msg.sender == owner, "Chi chu so huu moi co quyen");
-        _;
-    }
-
     function themSanPham(
         string memory maSanPham,
         string memory tenSanPham,
@@ -38,12 +40,14 @@ contract TruyXuatNguonGoc {
         string memory ipfsHash
     ) public chiChuSoHuu {
         require(bytes(danhSachSanPham[maSanPham].tenSanPham).length == 0, "San pham da ton tai");
+
         danhSachSanPham[maSanPham] = SanPham(
             tenSanPham,
             noiSanXuat,
             ngaySanXuat,
             ipfsHash
         );
+
         emit SanPhamMoi(maSanPham, tenSanPham, noiSanXuat, ngaySanXuat, ipfsHash);
     }
 
@@ -55,5 +59,55 @@ contract TruyXuatNguonGoc {
     ) {
         SanPham memory sp = danhSachSanPham[maSanPham];
         return (sp.tenSanPham, sp.noiSanXuat, sp.ngaySanXuat, sp.ipfsHash);
+    }
+
+    // ----------- PHẦN 2: TRẠNG THÁI VÀ NHẬT KÝ CHUỖI CUNG ỨNG -----------
+
+    enum TrangThai { SanXuat, VanChuyen, LuuKho, BanLe }
+
+    struct LichSuTrangThai {
+        TrangThai trangThai;
+        uint256 thoiGian;
+        string moTa;
+    }
+
+    mapping(string => LichSuTrangThai[]) public nhatKyTrangThai;
+
+    event CapNhatTrangThai(
+        string maSanPham,
+        TrangThai trangThai,
+        uint256 thoiGian,
+        string moTa
+    );
+
+    function capNhatTrangThai(
+        string memory maSanPham,
+        uint8 trangThaiMoi,
+        string memory moTa
+    ) public chiChuSoHuu {
+        require(bytes(danhSachSanPham[maSanPham].tenSanPham).length != 0, "San pham khong ton tai");
+        require(trangThaiMoi <= uint8(TrangThai.BanLe), "Trang thai khong hop le");
+
+        nhatKyTrangThai[maSanPham].push(
+            LichSuTrangThai(TrangThai(trangThaiMoi), block.timestamp, moTa)
+        );
+
+        emit CapNhatTrangThai(maSanPham, TrangThai(trangThaiMoi), block.timestamp, moTa);
+    }
+
+    function xemTrangThaiGanNhat(string memory maSanPham) public view returns (
+        TrangThai trangThai,
+        uint256 thoiGian,
+        string memory moTa
+    ) {
+        uint256 len = nhatKyTrangThai[maSanPham].length;
+        require(len > 0, "Chua co trang thai nao");
+
+        LichSuTrangThai memory cuoi = nhatKyTrangThai[maSanPham][len - 1];
+        return (cuoi.trangThai, cuoi.thoiGian, cuoi.moTa);
+    }
+
+    function demSoTrangThai(string memory maSanPham) public view returns (uint256) {
+        return nhatKyTrangThai[maSanPham].length;
     }
 }
